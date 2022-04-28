200913a5f1887525d0fd41d2617a3f5a5c47eaa4
墨雪遗痕 <2801067014@qq.com>
2022-04-28 10:45:45 +0800

    引入 elementplus

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+
+const getArray = <T, U>(param1: T, param2: U, times: number): [T, U][] => {
+  return new Array(times).fill([param1, param2]);
+};
+getArray<number,string>(1, "a", 3).forEach(item => {
+  console.log(item[1].length); // error 类型“number”上不存在属性“length”
+  console.log(item[0].toFixed(2)); // error 属性“toFixed”在类型“string”上不存在
+});
+
 </script>
 
 <template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,8 +26,7 @@ const count = ref(0)
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <el-button @click="count++" type="primary">count is: {{ count }}</el-button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
