6d84c85f66f61e011451fe83d8bfdc9f56087f80
墨雪遗痕 <2801067014@qq.com>
2022-05-13 22:13:16 +0800

    router

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/inksnow-blog',
+    path: '/inksnow-blog/',
     redirect:'/'
   },
   {
