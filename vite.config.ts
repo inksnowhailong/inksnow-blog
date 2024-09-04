9c59706f2b398800d77d839bab4c05895eeb3377
张海龙 <inksnowhailong@gmail.com>
2024-09-04 14:37:21 +0800

    新的开始，这个更好

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+export default defineConfig({
+  resolve: {
+    alias: {
+        '@types': path.resolve(__dirname, '../types'),
+        '@client': path.resolve(__dirname, '../client'),
+        '@utils': path.resolve(__dirname, '../client/utils'),
+        '@components': path.resolve(__dirname, '../client/components'),
+        '@composables': path.resolve(__dirname, '../client/composables'),
+      },
+  },
+});
